5334832da98e6be7a42eeff8c415b6afa93913c6
revianugrah <anugrahdani955@gmail.com>
2022-09-05 21:35:13 +0700

    tes
    tes

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Sobat Rantau!" << endl;
+    cout << "Nama : Revi Anugrah Rahmat Dani" << endl;
+    cout << "NIM : A11.2022.14126" << endl;
+    cout << "Alamat : Parungkamal RT 03 RW 04, Kec.Lumbir, Kab.Banyumas" << endl;
+    cout << "Asal Sekolah : SMA N 1 Wangon" << endl;
+    return 0;
+}
