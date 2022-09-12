3055f0e924358769746c50064c8623b647418804
revianugrah <anugrahdani955@gmail.com>
2022-09-12 21:25:05 +0700

    pertemuan 3
    pertemuan 3

---
+++ pertemuan3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    /*
+    printtf ("test");
+    integer
+    */
+    int x = 17;
+    //int untuk  bilangan bulat
+    //fariabel x harus sama dengan fariabel yang akan dikeluarkan
+    float t = 161.5;
+    //float untuk menampilkan bilangan pecahan (bilangan real)
+    char b = 'M';
+    //untuk menampilkan satu karalter saja
+    string UB = "XXL";
+    //untuk untuk menampilkan beberapa karakter atau kalimat
+    cout <<"Umur saya: "<< x;
+    cout << endl;
+    //untuk mengganti baris
+    cout <<"Tinggi Badan Saya: "<< t;
+    cout << endl;
+    cout <<"Ukuran Baju Saya: "<< b;
+    cout << endl;
+    cout <<"Ukuran Baju Teman Saya; "<< UB;
+
+    return 0;
+}
